1cd380f1b8accaec25119eff06ac935fb7840887
DanielGartzia <dgarcl11@estudiantes.unileon.es>
2023-11-16 11:55:29 +0100

    Vista de transaferencias completada

---
+++ app/frontend/src/app/services/transferOperations/transferOperations.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Transaction } from 'src/app/models/transaction.model';
+import {BankAccount} from 'src/app/models/bankAccount.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ViewTransactionService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+  getBankAccount(id : Number):Promise<any>{
+    var response = this.http.get<any>(`http://localhost:8080/accounts?idBankAccount=${id}`).toPromise();
+    return response;
+    /*this.http.get(`http://localhost:8080/accounts?idBankAccount=${id}` ,{ observe: 'response' }).subscribe((response: HttpResponse<any>) =>{
+    alert(JSON.stringify(response.body))
+    return response.toPromise();
+    })*/
+  }
+  showTransfer(bankAccount : any):Promise<any>{
+    alert("Transferencias pedidas de cuenta con id:"+bankAccount.id)
+    var response = this.http.get(`http://localhost:8080/accounts?id=${bankAccount.id}`).toPromise();
+    return response;
+  }
+}
--- app/frontend/src/app/transaction-view/transaction-view.component.ts
+++ app/frontend/src/app/transaction-view/transaction-view.component.ts
@@ -4,6 +4,9 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { HeaderComponentComponent } from '../header-component/header-component.component';
+import { ViewTransactionService } from '../services/transferOperations/transferOperations.service';
+import {finalize} from 'rxjs/operators';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'transaction-view',
@@ -11,13 +14,29 @@ import { HeaderComponentComponent } from '../header-component/header-component.c
   styleUrls: ['./transaction-view.component.css']
 })
 export class TransactionViewComponent {
-  displayedColumns: string[] = ['id','importe','numOrigen','numDestino','fecha','concepto']; // Lista de columnas a mostrar
-  dataSource: MatTableDataSource<any>; // Fuente de datos de la tabla
+  displayedColumns: string[] = ['id','importe','numOrigen','numDestino','fecha','concepto','estado']; // Lista de columnas a mostrar
+  dataSource: MatTableDataSource<any> = new MatTableDataSource; // Fuente de datos de la tabla
 
-  @Input() transactions : Transaction[] = [{id:0,importe:1000,numeroOrigen:0,numeroDestino:0,fecha:new Date(),concepto:"Prueba"}]
-  constructor() {
-    // Datos de ejemplo, reemplaza con tus datos reales
-    const data = this.transactions
-    this.dataSource = new MatTableDataSource(data);
+  @Input() transactions : Transaction[] = [{id:0,importe:1000,numeroOrigen:0,numeroDestino:0,fecha:new Date(),concepto:"Prueba",estado : "Pendiente"},{id:20,importe:1000,numeroOrigen:0,numeroDestino:0,fecha:new Date(),concepto:"Prueba",estado : "Pendiente"}]
+  constructor(private viewTransactionService: ViewTransactionService) {
+    this.renderTransactions()
+  }
+
+  async renderTransactions(){
+    var bankAccount : any
+    var userTransactions : any
+    try {
+      bankAccount = await this.viewTransactionService.getBankAccount(3);
+      alert("Hemos traido la cuenta");
+      userTransactions = await this.viewTransactionService.showTransfer(bankAccount);
+      // Continúa con la ejecución aquí
+    } catch (error) {
+      console.error('Error:', error);
+      // Maneja el error aquí si es necesario
+    }
+      alert("Ya tendriamos las transacciones")
+      this.transactions = userTransactions;
+      const data = this.transactions
+      this.dataSource = new MatTableDataSource(data);
   }
 }
