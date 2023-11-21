b98b657f764ca798f68e5b0863c593713e4eef97
mcastm12 <mcastm12@estudiantes.unileon.es>
2023-11-21 12:15:04 +0100

    arreglo

--- app/frontend/src/app/login/login.component.ts
+++ app/frontend/src/app/login/login.component.ts
@@ -16,10 +16,6 @@ export class LoginComponent {
 
   login() {
     this.userOperationsService.login(this.email, this.password);
-    this.conectarWebsocket();
   }
 
-  conectarWebsocket(){
-    this.websocketTransacciones.connect(this.email);
-  }
 }
